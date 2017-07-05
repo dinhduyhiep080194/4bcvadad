f6084790fc93a27ab70c63f37fe2cfec8ff7c2cc
Dinh Hiep <hiepmanager@Dinhs-Mac.local>
2017-07-05 13:41:09 +0800

    Initial Commit

---
+++ TableView_BaiMauMVC/TableViewCell.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbName: UILabel!
+    
+    func configure(name: String) {
+        self.lbName.text = name
+    }
+}
